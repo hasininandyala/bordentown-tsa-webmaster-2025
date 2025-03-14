0a90e4d4d68436f398b5f8e647a75222adbfab2e
Hasini Nandyala <hasinin1108@gmail.com>
2025-03-14 18:39:50 -0400

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<bordentown-tsa-webmaster-2025>/' 
+  base: '/bordentown-tsa-webmaster-2025/' 
 })
